5084d12c8bd93fe52ada3224c4ab38527ffd283c
Ranwala <Ravindra.Ranwala@pearson.com>
2019-06-20 10:22:51 +0530

    ch01 is completed.

---
+++ src/main/java/com/concurrency/jcip/ch1/UnsafeSequence.java
@@ -0,0 +1,21 @@
+package com.concurrency.jcip.ch1;
+
+import net.jcip.annotations.*;
+
+/**
+ * UnsafeSequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@NotThreadSafe
+public class UnsafeSequence {
+    private int value;
+
+    /**
+     * Returns a unique value.
+     */
+    public int getNext() {
+        return value++;
+    }
+}
